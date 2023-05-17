74cdd815ae5250c751126ec09345ef57d103077b
Linda Andersson <linda.29.andersson@gmail.com>
2023-05-17 15:25:18 +0200

    Homepage setup

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!
-      </Text>
+      <Text>Välkommstmeddlande här!</Text>
+      <TextInput>Sök efter bok eller författare...</TextInput>
       <StatusBar style="auto" />
     </View>
   );
