b92384fea3084e9f94fcfb28560e0ed2119911e6
Linda Andersson <linda.29.andersson@gmail.com>
2023-05-17 16:09:02 +0200

    Components setup for results

---
+++ components/Results.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Results = ({ input }) => {
+  return (
+    <View>
+      <Text>Results here!</Text>
+      <Text>{input}</Text>
+    </View>
+  );
+};
+
+export default Results;
